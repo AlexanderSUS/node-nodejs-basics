126603d6d09e05fd30da7ea7b6f0920e48dd911a
Alexander Suslov <sasha_suslov07@mail.ru>
2022-06-05 00:23:26 +0300

    feat: done Worker Threads subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,39 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import path from 'path';
+
 export const performCalculations = async () => {
-    // Write your code here
+    const WORKER_URL = path.resolve('wt', 'worker.js');
+    const VALUE = 10;
+    const result = [];
+    const cpus = os.cpus().length;
+
+    for (let i = 0; i < cpus; i++) {
+        const index = i;
+
+        result[index] = new Promise((resolve, reject) => {
+
+            const worker = new Worker(WORKER_URL, {
+                workerData: VALUE + index,
+            });
+
+            worker.on('message', value => {
+                resolve({ status: 'resolved',
+                    data: value,
+                });
+            });
+
+            worker.on('error', () => {
+                reject({
+                    status: 'error',
+                    data: null,
+                });
+            });
+        });
+    }
+
+    return Promise.all(result);
 };
+
+performCalculations()
+    .then(data => console.log(data));
