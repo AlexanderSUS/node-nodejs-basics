a96d89664d1ace1ed13ef6c820ea694442e370a3
Alexander Suslov <sasha_suslov07@mail.ru>
2022-06-04 21:21:42 +0300

    feat: done zip subtask

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,20 @@
+import fs from 'fs';
+import { resolve } from 'path';
+import zlib from 'zlib';
+
+
 export const decompress = async () => {
-    // Write your code here 
+  const TARGET_DIR = 'src/zip/files';
+  const COMPRESSED_FILE = 'archive.gz';
+  const UNCOMPRESSED_FILE = 'fileToCompress.txt';
+
+  const gzip = zlib.createGunzip();
+  const getFileUrl = (fileName) => resolve(TARGET_DIR, fileName);
+
+  const input = fs.createReadStream(getFileUrl(COMPRESSED_FILE))
+  const output = fs.createWriteStream(getFileUrl(UNCOMPRESSED_FILE))
+
+  input.pipe(gzip).pipe(output);
 };
+
+decompress();
