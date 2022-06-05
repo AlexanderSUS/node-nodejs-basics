c780667853aa3f56fe36489d765d7ad37862b6b5
Alexander Suslov <sasha_suslov07@mail.ru>
2022-06-05 23:33:06 +0300

    fix: imports

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,5 @@
 export const parseArgs = () => {
-    const args = proces.argv.slice(2)
+    const args = process.argv.slice(2)
 
     for (let i = 0; i < args.length; i++) {
         console.log(`${args[i].slice(2)} is ${args[++i]}`);
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
